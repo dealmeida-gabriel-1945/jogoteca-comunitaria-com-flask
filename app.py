c4e680830a8fffb600ad371729005a276ca830d2
dealmeida-gabriel-1945 <dealmeida.gabriel.d@gmail.com>
2021-10-22 00:45:15 -0300

    Iniciando a feature de criação de conta

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, session, flash
 import json
+from types import SimpleNamespace
 
 app = Flask(__name__)
 app.secret_key = 'IU+ta5:9~0vZ,*}'
@@ -16,6 +17,12 @@ class Conta:
     def __init__(self, usuario, senha):
         self.usuario = usuario
         self.senha = senha
+        self.jogos = []
+
+    def as_dict(self):
+        dict = self.__dict__
+        dict["jogos"] = [jogo.__dict__ for jogo in dict["jogos"]]
+        return dict
 
 
 lista = [
@@ -39,7 +46,7 @@ def render_lista_jogos():
         'lista.html',
         page_title='Jogoteca',
         titulo='Jogos',
-        jogos=lista
+        jogos=get_usuario_logado().jogos
     )
 
 
@@ -55,13 +62,15 @@ def render_cadastro_jogo():
 
 @app.route('/create', methods=['POST'])
 def create_jogo():
-    lista.append(
+    usuario_logado = get_usuario_logado()
+    usuario_logado.jogos.append(
         Jogo(
             request.form['nome'],
             request.form['categoria'],
             request.form['console']
         )
     )
+    save_usuario_logado(usuario_logado)
     return redirect('/')
 
 
@@ -77,11 +86,11 @@ def autenticar():
     matches = [
         conta
         for conta in contas
-        if conta.usuario == request.form['senha']
-           and conta.usuario == request.form['usuario']
+        if conta.usuario == request.form['usuario']
+           and conta.senha == request.form['senha']
     ]
     if len(matches) == 1:
-        session['usuario_logado'] = json.dumps(matches[0].__dict__)
+        session['usuario_logado'] = json.dumps(parse_conta_from_session(matches[0]).as_dict())
         return redirect('/')
     else:
         flash('Usuário e(ou) senha inválido(s)!')
@@ -92,6 +101,34 @@ def autenticar():
 def deslogar():
     session['usuario_logado'] = None
     return redirect('/login')
+
+
+def get_usuario_logado():
+    return json.loads(
+        session['usuario_logado'],
+        object_hook=lambda d: SimpleNamespace(**d)
+    )
+
+
+def save_usuario_logado(to_save):
+    conta_aux = Conta(to_save.usuario, to_save.senha)
+    conta_aux.jogos = to_save.jogos
+    session['usuario_logado'] = json.dumps(conta_aux.as_dict())
+    aux = list()
+    for conta in contas:
+        if conta.usuario == to_save.usuario and conta.senha == to_save.senha:
+           aux.append(to_save)
+        else:
+            aux.append(conta)
+    contas.clear()
+    contas.extend(aux)
+    print('conta salva')
+
+
+def parse_conta_from_session(to_parse):
+    to_return = Conta(to_parse.usuario, to_parse.senha)
+    to_return.jogos = to_parse.jogos
+    return to_return
 
 
 if __name__ == '__main__':
